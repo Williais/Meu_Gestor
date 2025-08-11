2e854b75fbcf78bcffb953fc5b3e1de7762d31e8
Willian Padilha <45665895+Williais@users.noreply.github.com>
2025-08-11 02:14:53 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,151 @@
+
+// Selecionando os elementos do DOM
+const entrada = document.querySelector('#entrada')
+const saida = document.querySelector('#saida')
+const saldo = document.querySelector('#saldo')
+const titleSaldo = document.querySelector('#title-saldo')
+
+// Selecionando os elementos do formulário para adicionar nos elementos do DOM
+const form = document.querySelector('#formulario')
+const descricaoInput = document.querySelector('#descricao')
+const valorInput = document.querySelector('#valor')
+const tipoInput = document.querySelector('#tipo')
+
+// Selecionando a lista onde as transações serão exibidas
+const listaUl = document.querySelector('#listagem');
+
+// Criando o LocalStorage
+const transacaoSalva = localStorage.getItem('HistoricoFinanceiro')
+
+//se existir alguma coisa no localStorage, vai retornar esse valor em convertido. senao, retorna um array vazio
+let transacoes = transacaoSalva ? JSON.parse(transacaoSalva) : [] // array onde todas as transações serão adicionadas
+
+atualizarPainelResumo() //chamei a função uma vez no início para garantir que os dados carregados do localStorage sejam exibidos na tela assim que a página abre.
+
+// Adicionando um evento de submit ao formulário
+form.addEventListener('submit', function (e){
+    //isso fará que a página não recarregue ao enviar o formulário
+    e.preventDefault()
+
+    // Pegando os valores dos inputs
+    const descricao = descricaoInput.value
+    const valor = parseFloat(valorInput.value)
+    const tipo = tipoInput.value
+
+    // Validação simples para não adicionar transações vazias
+    if (descricao.trim() === '' || isNaN(valor)) { // Trim para remover espaços em branco e isNaN para verificar se o valor é um número
+        alert( 'PLS, coloque uma descrição e um valor válido.' )
+        return // Se a descrição estiver vazia ou o valor não for um número, não adiciona a transação
+    }
+
+    // Criando um objeto para a nova transação
+    const novaTransacao = {
+        id: Date.now(), // Usando o timestamp como ID único
+        descricao: descricao,
+        valor: valor,
+        tipo: tipo
+    }
+
+    // Adicionando a nova transação ao array de transações
+    transacoes.push(novaTransacao)
+
+    atualizarPainelResumo() // Atualiza o painel de resumo
+
+    // limpando os campos do formulário
+    descricaoInput.value = ''
+    valorInput.value = ''
+    descricaoInput.focus() // Foca no campo de descrição para facilitar
+
+})
+
+// Função para atualizar o painel de resumo
+
+function atualizarPainelResumo() {
+    renderizarLista() // Renderiza as transações na lista
+    atualizarResumo() // Atualiza os valores de entradas, saídas e saldo
+
+    //Toda vez que atualizamos a tela, salvamos a versão mais recente do array no localStorage
+    localStorage.setItem('HistoricoFinanceiro', JSON.stringify(transacoes)) // traduzi para texto
+}
+
+function renderizarLista(){
+    // limpando a lista antes de renderizar novamente
+    listaUl.innerHTML = ''
+
+    // remover item de exemplo que estava na lista
+    const itemDeExemplo = document.querySelector('.itens') 
+    if (itemDeExemplo) {itemDeExemplo.remove()}
+
+
+    // agora vem a magica de renderizar a lista de transações e adicionar os itens na lista
+
+    transacoes.forEach(transacoes => {
+        // vou criar o elemento <li>
+        const li = document.createElement('li')
+        li.classList.add('itens') // Adicionando a classe 'itens' para estilização futura
+
+        //criando outra classe 'Entrada' ou 'Saída' para estilização no CSS
+        li.classList.add(transacoes.tipo)
+
+        // formatar o valor para moeda brasileira (R$)
+        const valorFormatado = transacoes.valor.toLocaleString('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+        })
+
+
+     
+        // criar <li> na area do historico
+
+        li.innerHTML = `
+        <p> ${transacoes.descricao} </p>
+        <p> ${valorFormatado} </p>
+        <p> ${transacoes.tipo === 'entrada' ? 'Entrada <Strong>⇑</Strong>' : 'Saída <Strong>⇓</Strong>'} </p>`
+
+        listaUl.appendChild(li)
+
+    })
+
+}
+
+function atualizarResumo() {
+
+    // vou atualizar o painel de entrada
+
+    const painelEntrada = transacoes
+    .filter(trans => trans.tipo === 'entrada') // aq o filter eh como uma esteira que verifica se o valor que ta em trans (que eu criei, mas eh de transação) eh identico a 'entrada'. Se for, ele vai pra proxima esteira.
+    .reduce((acc, trans) => acc + trans.valor, 0) // essa esteira cria duas variaveis (acc, trans) para fazer a soma entre eles
+    // 'acc' É a abreviação de "accumulator". Pense como o visor da sua calculadora. Ele guarda o total acumulado até o momento. Ele começa com o valor inicial que definimos (0)
+
+    // vou atualizar o painel de saida
+
+    const painelSaida = transacoes
+    .filter(trans => trans.tipo === 'saida')
+    .reduce((acc, trans) => acc + trans.valor, 0)
+
+    const painelSaldo = painelEntrada - painelSaida
+
+    //atualizar paineis atraves do html. vou imprimir o valor ja convertido para o Real
+
+    entrada.textContent = painelEntrada.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+    saida.textContent = painelSaida.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+    saldo.textContent = painelSaldo.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL'
+    })
+
+    // alterar cor no caso de total ser negativo e positivo
+    if(painelSaldo < 0) {
+        saldo.style.color = '#9b2323'
+        titleSaldo.style.color = '#9b2323'
+    }else {
+        saldo.style.color = 'green'
+        titleSaldo.style.color = 'green'
+    }
+}
